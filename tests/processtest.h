fd4644f1ca116cfcde401474bc11c4dafea94a0d
Pino Toscano <pino@kde.org>
2007-03-01 11:12:17 +0000

    move the class definition in a .h, so it can get moc'ed properly; add a main using the QTEST_KDE
    svn path=/branches/work/kdehw/solidstats/tests/; revision=638265

--- tests/processtest.h
+++ tests/processtest.h
@@ -16,28 +16,17 @@
     Boston, MA 02110-1301, USA.
 */
 
+#ifndef TESTPROCESS_H
+#define TESTPROCESS_H
 
-#include <QtTest>
-#include <QtCore>
+#include <QtCore/QObject>
 
-#include <klocale.h>
-#include <qtest_kde.h>
+class testProcess: public QObject
+{
+    Q_OBJECT
+      private slots:
+        void testProcesses();
+};
 
-#include "solidstats/processes.h"
-#include "solidstats/process.h"
-
-#include "processtest.h"
-
-void testProcess::testProcesses() {
-	QHash<long, Solid::Process *> processes = Solid::Processes::getProcesses();
-	foreach( Solid::Process *process, processes)
-		kDebug() << process->name << endl;
-
-	QHash<long, Solid::Process *> processes2 = Solid::Processes::getProcesses();
-	QCOMPARE(processes, processes2); //Make sure calling it twice gives the same results.  The difference is time is so small that it really shouldn't have changed
-}
-
-QTEST_KDEMAIN(testProcess, NoGUI)
-
-#include "processtest.moc"
+#endif
 
