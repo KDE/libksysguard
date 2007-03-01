1479ced8ebba54592333614d6b5d8d7b9cbf99b3
John Tapsell <john.tapsell@kdemail.net>
2007-03-01 12:25:33 +0000

    Test now passes
    svn path=/branches/work/kdehw/solidstats/tests/; revision=638279

--- tests/processtest.cpp
+++ tests/processtest.cpp
@@ -30,8 +30,9 @@
 
 void testProcess::testProcesses() {
 	QHash<long, Solid::Process *> processes = Solid::Processes::getProcesses();
+	kDebug() << "Found " << processes.size() << endl;
 	foreach( Solid::Process *process, processes)
-		kDebug() << process->name << endl;
+		kDebug() << process->pid << endl;
 
 	QHash<long, Solid::Process *> processes2 = Solid::Processes::getProcesses();
 	QCOMPARE(processes, processes2); //Make sure calling it twice gives the same results.  The difference is time is so small that it really shouldn't have changed
