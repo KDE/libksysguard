69473a95b3d60f4ddb6662f48e49c25642f3c9dc
John Tapsell <john.tapsell@kdemail.net>
2007-11-15 00:36:40 +0000

    Change the licensing to LGPLv2 or later in the files that I own solely.  Greg Martyn has also given permission, just incase.
    svn path=/trunk/KDE/kdebase/workspace/; revision=736855

--- processcore/processes_linux_p.cpp
+++ processcore/processes_linux_p.cpp
@@ -1,9 +1,11 @@
 /*  This file is part of the KDE project
+    
     Copyright (C) 2007 John Tapsell <tapsell@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
-    License version 2 as published by the Free Software Foundation.
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,6 +16,7 @@
     along with this library; see the file COPYING.LIB.  If not, write to
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
+
 */
 
 #include "processes_local_p.h"
