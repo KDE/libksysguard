0a0e9ecc16e02d6de01aa3ccd2129e8d7fcf7479
John Tapsell <john.tapsell@kdemail.net>
2007-11-17 20:16:03 +0000

    Add mHost
    svn path=/trunk/KDE/kdebase/workspace/; revision=738054

--- processui/ProcessModel_p.h
+++ processui/ProcessModel_p.h
@@ -1,3 +1,24 @@
+/*
+    KSysGuard, the KDE System Guard
+
+	Copyright (c) 2006-2007 John Tapsell <john.tapsell@kde.org>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
 #ifndef PROCESSMODEL_P_H_
 #define PROCESSMODEL_P_H_
 
@@ -154,6 +175,9 @@ public:
 
 	/** When displaying memory sizes, this is the units it should be displayed in */
 	int mUnits;
+
+	/** The hostname */
+	QString mHost;
 
 	ProcessModel* q;
 };
