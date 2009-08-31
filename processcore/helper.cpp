58ffb0217c03090daee3666ceac2174cfaad051b
John Tapsell <john.tapsell@kdemail.net>
2009-08-31 22:56:15 +0000

    Add a helper for kauth
    svn path=/trunk/KDE/kdebase/workspace/; revision=1017927

---
+++ processcore/helper.cpp
@@ -0,0 +1,46 @@
+/*
+    KSysGuard, the KDE System Guard
+
+	Copyright (c) 2009 John Tapsell <john.tapsell@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+
+*/
+
+#include "helper.h"
+#include <QDebug>
+#include "processes_local_p.h"
+
+
+/* The functions here run as ROOT.  So be careful. */
+
+KAuth::ActionReply KSysGuardProcessListHelper::sendsignal(QVariantMap parameters) {
+    qDebug() << "HERERERER";
+    KSysGuard::ProcessesLocal processes;
+    QList< long long> pids = qvariant_cast<QList<long long> >(parameters.value("pids"));
+    int signal = qvariant_cast<int>(parameters.value("signal"));
+    bool success = true;
+    for (int i = 0; i < pids.size(); ++i) {
+        success = processes.sendSignal(pids.at(i), signal);
+    }
+    if(success)
+        return KAuth::ActionReply::SuccessReply;
+    else
+        return KAuth::ActionReply::HelperErrorReply;
+}
+
+KDE4_AUTH_HELPER_MAIN("org.kde.ksysguardprocesslisthelper", KSysGuardProcessListHelper);
+
