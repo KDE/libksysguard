5992fb4c2ee5a6ee72432a766e33980529f79ab7
Marco Martin <notmart@gmail.com>
2020-07-08 17:34:10 +0200

    delete the face config ui when face gets switched
    don't let face config uis linger there when the user switches the face.
    also, not all face have a config ui. Don't load anything at all if
    not provided by the face.
    
    BUG:423071

--- faces/SensorFaceController.cpp
+++ faces/SensorFaceController.cpp
@@ -395,6 +395,10 @@ void SensorFaceController::setFaceId(const QString &face)
         d->compactRepresentation->deleteLater();
         d->fullRepresentation.clear();
     }
+    if (d->faceConfigUi) {
+        d->faceConfigUi->deleteLater();
+        d->faceConfigUi.clear();
+    }
 
     d->faceId = face;
 
@@ -480,10 +484,20 @@ QQuickItem *SensorFaceController::faceConfigUi()
         return d->faceConfigUi;
     }
 
+    const QString filePath = d->facePackage.filePath("ui", QStringLiteral("Config.qml"));
+
+    if (filePath.isEmpty()) {
+        return nullptr;
+    }
+
     d->faceConfigUi = d->createConfigUi(QStringLiteral(":/FaceDetailsConfig.qml"),
     {{QStringLiteral("controller"), QVariant::fromValue(this)},
-         {QStringLiteral("source"), d->facePackage.filePath("ui", QStringLiteral("Config.qml"))}});
+         {QStringLiteral("source"), filePath}});
 
+    if (d->faceConfigUi && !d->faceConfigUi->property("item").value<QQuickItem *>()) {
+        d->faceConfigUi->deleteLater();
+        d->faceConfigUi.clear();
+    }
     return d->faceConfigUi;
 }
 
