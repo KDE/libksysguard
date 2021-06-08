ff8972e562d457bc3ccf6ac5e15948f7e46a6078
Ömer Fadıl Usta <omerusta@gmail.com>
2021-06-08 08:17:36 +0000

    Fix cgrouptest

--- autotests/cgrouptest.cpp
+++ autotests/cgrouptest.cpp
@@ -16,40 +16,41 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
 */
-
-#include "cgrouptest.h"
-
+#include <QTest>
+#include <QObject>
 #define private public
 #include "cgroup.h"
 #define private private
 
-#include <QTest>
+class CGroupTest : public QObject {
+    Q_OBJECT
 
-void CGroupTest::testAppUnitRegex_data()
-{
-    QTest::addColumn<QString>("id");
-    QTest::addColumn<QString>("desktopName");
-    QTest::newRow("service") << "app-gnome-org.gnome.Evince@12345.service" << "org.gnome.Evince";
-    QTest::newRow("service .desktop") << "app-flatpak-org.telegram.desktop@12345.service" << "org.telegram.desktop";
-    QTest::newRow("service no launcher") << "app-org.kde.okular@12345.service" << "org.kde.okular";
-    QTest::newRow("service no random") << "app-KDE-org.kde.okular.service" << "org.kde.okular";
-    QTest::newRow("service no launcher no random") << "app-org.kde.amarok.service" << "org.kde.amarok";
-    QTest::newRow("scope") << "app-gnome-org.gnome.Evince-12345.scope" << "org.gnome.Evince";
-    QTest::newRow("scope no launcher") << "app-org.gnome.Evince-12345.scope" << "org.gnome.Evince";
-}
-
-void CGroupTest::testAppUnitRegex()
-{
-    QFETCH(QString, id);
-    QFETCH(QString, desktopName);
-    KSysGuard::CGroup c(id);
-    if (c.service()->menuId().isEmpty()) {
-        // The service is not known on this machine and we constructed a service with the id as name
-        QCOMPARE(c.service()->name(), desktopName);
-    } else {
-        QCOMPARE(c.service()->desktopEntryName(), desktopName.toLower());
+private Q_SLOTS:
+    void testAppUnitRegex_data()
+    {
+        QTest::addColumn<QString>("id");
+        QTest::addColumn<QString>("desktopName");
+        QTest::newRow("service") << "app-gnome-org.gnome.Evince@12345.service" << "org.gnome.Evince";
+        QTest::newRow("service .desktop") << "app-flatpak-org.telegram.desktop@12345.service" << "org.telegram.desktop";
+        QTest::newRow("service no launcher") << "app-org.kde.okular@12345.service" << "org.kde.okular";
+        QTest::newRow("service no random") << "app-KDE-org.kde.okular.service" << "org.kde.okular";
+        QTest::newRow("service no launcher no random") << "app-org.kde.amarok.service" << "org.kde.amarok";
+        QTest::newRow("scope") << "app-gnome-org.gnome.Evince-12345.scope" << "org.gnome.Evince";
+        QTest::newRow("scope no launcher") << "app-org.gnome.Evince-12345.scope" << "org.gnome.Evince";
     }
-}
 
-QTEST_MAIN(CGroupTest)
+    void testAppUnitRegex()
+    {
+        QFETCH(QString, id);
+        QFETCH(QString, desktopName);
+        KSysGuard::CGroup c(id);
+        if (c.service()->menuId().isEmpty()) {
+            // The service is not known on this machine and we constructed a service with the id as name
+            QCOMPARE(c.service()->name(), desktopName);
+        } else {
+            QCOMPARE(c.service()->desktopEntryName(), desktopName.toLower());
+        }
+    }
+};
+QTEST_MAIN(CGroupTest);
 #include "cgrouptest.moc"
