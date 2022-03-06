KDE/libksysguard

commit d4d8a5a28d22d4d2958052738c2f4110955b47af

Port more deprecated methods

processcore/extended_process_list.cpp
@@ -555,17 +555,12 @@ void ExtendedProcesses::Private::loadPlugins()
     // instantiate all plugins
     for (const auto &pluginMetaData : listMetaData) {
         qCDebug(LIBKSYSGUARD_PROCESSCORE) << "loading plugin" << pluginMetaData.name();
-        auto factory = qobject_cast<KPluginFactory *>(pluginMetaData.instantiate());
-        if (!factory) {
-            qCCritical(LIBKSYSGUARD_PROCESSCORE) << "failed to load plugin factory" << pluginMetaData.name();
-            continue;
-        }
-        ProcessDataProvider *provider = factory->create<ProcessDataProvider>(q);
-        if (!provider) {
+        auto provider = KPluginFactory::instantiatePlugin<ProcessDataProvider>(pluginMetaData);
+        if (!provider.plugin) {
             qCCritical(LIBKSYSGUARD_PROCESSCORE) << "failed to instantiate ProcessDataProvider" << pluginMetaData.name();
             continue;
         }
-        m_providers << provider;
+        m_providers << provider.plugin;
     }
 }
 

processcore/process_attribute_model.cpp
@@ -68,13 +68,13 @@ QVariant ProcessAttributeModel::data(const QModelIndex &index, int role) const
 
 QHash<int, QByteArray> ProcessAttributeModel::roleNames() const
 {
-    return QAbstractListModel::roleNames().unite({
-        {static_cast<int>(Role::Id), "id"},
-        {static_cast<int>(Role::Name), "name"},
-        {static_cast<int>(Role::ShortName), "shortName"},
-        {static_cast<int>(Role::Description), "description"},
-        {static_cast<int>(Role::Unit), "unit"},
-        {static_cast<int>(Role::Minimum), "minimum"},
-        {static_cast<int>(Role::Maximum), "maximum"},
-    });
+    QHash<int, QByteArray> roles = QAbstractListModel::roleNames();
+    roles.insert(static_cast<int>(Role::Id), "id");
+    roles.insert(static_cast<int>(Role::Name), "name");
+    roles.insert(static_cast<int>(Role::ShortName), "shortName");
+    roles.insert(static_cast<int>(Role::Description), "description");
+    roles.insert(static_cast<int>(Role::Unit), "unit");
+    roles.insert(static_cast<int>(Role::Minimum), "minimum");
+    roles.insert(static_cast<int>(Role::Maximum), "maximum");
+    return roles;
 }

commit ae73bd15d1b8327797e6e2c27cc7ad415f4d802e

[Extended Processes] Pass parent to created plugin
The plugin relies on its parent to be `KSysGuard::Processes`.

Regression from d4d8a5a28d22d4d2958052738c2f4110955b47af

BUG: 451148

processcore/extended_process_list.cpp
@@ -555,7 +555,7 @@ void ExtendedProcesses::Private::loadPlugins()
     // instantiate all plugins
     for (const auto &pluginMetaData : listMetaData) {
         qCDebug(LIBKSYSGUARD_PROCESSCORE) << "loading plugin" << pluginMetaData.name();
-        auto provider = KPluginFactory::instantiatePlugin<ProcessDataProvider>(pluginMetaData);
+        auto provider = KPluginFactory::instantiatePlugin<ProcessDataProvider>(pluginMetaData, q);
         if (!provider.plugin) {
             qCCritical(LIBKSYSGUARD_PROCESSCORE) << "failed to instantiate ProcessDataProvider" << pluginMetaData.name();
             continue;