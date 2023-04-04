73ff7425a8ad1e5d0c340affb2deb59e5b91a145
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-04-04 20:20:44 +0200

    Remove translated strings from package structure definitions
    We do not use them anywhere.
    https://invent.kde.org/frameworks/kpackage/-/merge_requests/74 removed the parameter from the method calls.

--- faces/packagestructure/sensorfacepackage.cpp
+++ faces/packagestructure/sensorfacepackage.cpp
@@ -22,26 +22,20 @@ public:
     {
         package->setDefaultPackageRoot(QStringLiteral("ksysguard/sensorfaces"));
 
-        package->addDirectoryDefinition("ui", QStringLiteral("ui"), i18n("User Interface"));
+        package->addDirectoryDefinition("ui", QStringLiteral("ui"));
 
-        package->addFileDefinition("CompactRepresentation",
-                                   QStringLiteral("ui/CompactRepresentation.qml"),
-                                   i18n("The compact representation of the sensors plasmoid when collapsed, for instance in a panel."));
+        package->addFileDefinition("CompactRepresentation", QStringLiteral("ui/CompactRepresentation.qml"));
         package->setRequired("CompactRepresentation", true);
 
-        package->addFileDefinition("FullRepresentation",
-                                   QStringLiteral("ui/FullRepresentation.qml"),
-                                   i18n("The representation of the plasmoid when it's fully expanded."));
+        package->addFileDefinition("FullRepresentation", QStringLiteral("ui/FullRepresentation.qml"));
         package->setRequired("FullRepresentation", true);
 
-        package->addFileDefinition("ConfigUI", QStringLiteral("ui/Config.qml"), i18n("The optional configuration page for this face."));
+        package->addFileDefinition("ConfigUI", QStringLiteral("ui/Config.qml"));
 
-        package->addDirectoryDefinition("config", QStringLiteral("config"), i18n("Configuration support"));
-        package->addFileDefinition("mainconfigxml", QStringLiteral("config/main.xml"), i18n("KConfigXT xml file for face-specific configuration options."));
+        package->addDirectoryDefinition("config", QStringLiteral("config"));
+        package->addFileDefinition("mainconfigxml", QStringLiteral("config/main.xml"));
 
-        package->addFileDefinition("FaceProperties",
-                                   QStringLiteral("faceproperties"),
-                                   i18n("The configuration file that describes face properties and capabilities."));
+        package->addFileDefinition("FaceProperties", QStringLiteral("faceproperties"));
         package->setRequired("FaceProperties", true);
     }
 };
