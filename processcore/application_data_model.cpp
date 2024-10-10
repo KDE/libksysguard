96cd4ed58e984fc1f2125c63b0e93dea8c427c48
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-10-10 14:24:58 +0200

    processcore: Make ApplicationDataModel aggregate similar CGroups into one entry
    This changes ApplicationDataModel to aggregate similar CGroups into one
    application entry.
    
    The primary motivation for this is that a single Flatpak application
    instance may spawn several separate CGroups that we would list as
    separate applications before this change, but which logically belong to
    the same application. Upstream has indicated this behaviour is
    intentional and seems to have no inclination on adding anything to allow
    us to identify these cases, so we are left with just merging everything.
    
    This is also slightly more aggressive in filtering out some things that
    are currently listed but aren't really applications, most notably some
    D-Bus autostarted applications.
    
    BUG: 481551

--- processcore/application_data_model.cpp
+++ processcore/application_data_model.cpp
@@ -5,25 +5,119 @@
 */
 
 #include "application_data_model.h"
+
 #include <KUser>
 
 #include <QDebug>
 
+#include "cgroup.h"
+
 using namespace KSysGuard;
+using namespace Qt::StringLiterals;
 
 ApplicationDataModel::ApplicationDataModel(QObject *parent)
     : CGroupDataModel(QStringLiteral("/user.slice/user-%1.slice/user@%1.service").arg(KUserId::currentEffectiveUserId().toString()), parent)
 {
 }
 
+ApplicationDataModel::~ApplicationDataModel() = default;
+
 bool ApplicationDataModel::filterAcceptsCGroup(CGroup *cgroup)
 {
-    if (!CGroupDataModel::filterAcceptsCGroup(cgroup->id())) {
+    if (!isCgroupRelevant(cgroup)) {
         return false;
     }
 
-    // this class is all temporary. In the future as per https://systemd.io/DESKTOP_ENVIRONMENTS/
-    // all apps will have a managed by a drop-in that puts apps in the app.slice
-    // when this happens adjust the root above and drop this filterAcceptsCGroup line
-    return cgroup->id().contains(QLatin1String("/app-")) || (cgroup->id().contains(QLatin1String("/flatpak")) && cgroup->id().endsWith(QLatin1String("scope")));
+    auto itr = m_applications.find(applicationId(cgroup));
+    if (itr == m_applications.end()) {
+        return false;
+    }
+
+    // Only display an entry for the first item in an application's cgroup set.
+    if (itr.value().cgroups.empty() || cgroup != itr.value().cgroups.first()) {
+        return false;
+    }
+
+    return true;
+}
+
+void ApplicationDataModel::cgroupAdded(CGroup *cgroup)
+{
+    if (!isCgroupRelevant(cgroup)) {
+        return;
+    }
+
+    auto appId = applicationId(cgroup);
+
+    auto itr = m_applications.find(appId);
+    if (itr == m_applications.end()) {
+        itr = m_applications.emplace(appId);
+        itr.value().applicationId = appId;
+    }
+
+    itr.value().cgroups.append(cgroup);
+}
+
+void ApplicationDataModel::cgroupRemoved(CGroup *cgroup)
+{
+    auto itr = m_applications.find(applicationId(cgroup));
+    if (itr == m_applications.end()) {
+        return;
+    }
+
+    auto &application = itr.value();
+
+    application.cgroups.removeOne(cgroup);
+
+    if (itr.value().cgroups.empty()) {
+        m_applications.erase(itr);
+    }
+}
+
+QList<Process *> ApplicationDataModel::processesFor(CGroup *cgroup) const
+{
+    auto itr = m_applications.find(applicationId(cgroup));
+    if (itr == m_applications.end() || itr.value().cgroups.isEmpty()) {
+        return {};
+    }
+
+    QList<Process *> result;
+    for (auto cgroup : std::as_const(itr.value().cgroups)) {
+        result.append(CGroupDataModel::processesFor(cgroup));
+    }
+
+    return result;
+}
+
+bool ApplicationDataModel::isCgroupRelevant(CGroup *cgroup)
+{
+    if (!CGroupDataModel::filterAcceptsCGroup(cgroup)) {
+        return false;
+    }
+
+    auto cgroupId = cgroup->id();
+    if (!cgroupId.contains(QLatin1String("/app-")) && !(cgroup->id().contains(QLatin1String("/flatpak")) && cgroup->id().endsWith(QLatin1String("scope")))) {
+        return false;
+    }
+
+    auto appId = applicationId(cgroup);
+    // Certain DBus launched things will end up creating a CGroup that looks like
+    // dbus-:1.2-org.telegram.desktop@0.service . Since these don't actually contain
+    // anything relevant, just filter them out as they end up just cluttering the
+    // list.
+    if (appId.startsWith(u"dbus-:"_s)) {
+        return false;
+    }
+
+    return true;
+}
+
+QString ApplicationDataModel::applicationId(CGroup *cgroup) const
+{
+    auto appId = cgroup->service()->storageId();
+    if (appId.isEmpty()) {
+        appId = cgroup->service()->name();
+    }
+
+    return appId;
 }
--- processcore/application_data_model.h
+++ processcore/application_data_model.h
@@ -12,12 +12,35 @@
 
 namespace KSysGuard
 {
+class Process;
+
+/**
+ * This is a subclass of CGroupDataModel that combines separate CGroups with
+ * matching application IDs into a single row.
+ */
 class PROCESSCORE_EXPORT ApplicationDataModel : public CGroupDataModel
 {
     Q_OBJECT
 public:
     ApplicationDataModel(QObject *parent = nullptr);
+    ~ApplicationDataModel() override;
+
+protected:
     bool filterAcceptsCGroup(CGroup *cgroup) override;
+    void cgroupAdded(CGroup *cgroup) override;
+    void cgroupRemoved(CGroup *cgroup) override;
+    QList<Process *> processesFor(CGroup *cgroup) const override;
+
+private:
+    struct Application {
+        QString applicationId;
+        QList<CGroup *> cgroups;
+    };
+
+    bool isCgroupRelevant(CGroup *cgroup);
+    QString applicationId(CGroup *cgroup) const;
+
+    QHash<QString, Application> m_applications;
 };
 
 }
