2345ba1c023751b41df13b35170a3c57c68ce38e
Arjen Hiemstra <ahiemstra@heimr.nl>
2025-03-14 15:53:18 +0100

    sensors: Use u""_s string literal syntax for strings in SensorGroup

--- sensors/SensorGroup.cpp
+++ sensors/SensorGroup.cpp
@@ -10,6 +10,8 @@
 #include <QDebug>
 #include <QRegularExpression>
 
+using namespace Qt::StringLiterals;
+
 namespace KSysGuard
 {
 SensorGroup::SensorGroup()
@@ -26,38 +28,38 @@ void SensorGroup::retranslate()
     m_sensorNames.clear();
     m_segmentNames.clear();
 
-    m_sensorNames[QStringLiteral("cpu/cpu\\d+/usage")] = i18nc("Total load sensor of all cores", "[Group] Total Usage");
-    m_sensorNames[QStringLiteral("cpu/cpu\\d+/user")] = i18nc("All cores user load sensors", "[Group] User Usage");
-    m_sensorNames[QStringLiteral("cpu/cpu\\d+/system")] = i18nc("All cores user system sensors", "[Group] System Usage");
-    m_sensorNames[QStringLiteral("cpu/cpu\\d+/wait")] = i18nc("All cores wait load sensors", "[Group] Wait Usage");
-    m_sensorNames[QStringLiteral("cpu/cpu\\d+/frequency")] = i18nc("All cores clock frequency sensors", "[Group] Clock Frequency");
-    m_sensorNames[QStringLiteral("cpu/cpu\\d+/temperature")] = i18nc("All cores temperature sensors", "[Group] Temperature");
-    m_sensorNames[QStringLiteral("cpu/cpu\\d+/name")] = i18nc("All cores names", "[Group] Name");
+    m_sensorNames[u"cpu/cpu\\d+/usage"_s] = i18nc("Total load sensor of all cores", "[Group] Total Usage");
+    m_sensorNames[u"cpu/cpu\\d+/user"_s] = i18nc("All cores user load sensors", "[Group] User Usage");
+    m_sensorNames[u"cpu/cpu\\d+/system"_s] = i18nc("All cores user system sensors", "[Group] System Usage");
+    m_sensorNames[u"cpu/cpu\\d+/wait"_s] = i18nc("All cores wait load sensors", "[Group] Wait Usage");
+    m_sensorNames[u"cpu/cpu\\d+/frequency"_s] = i18nc("All cores clock frequency sensors", "[Group] Clock Frequency");
+    m_sensorNames[u"cpu/cpu\\d+/temperature"_s] = i18nc("All cores temperature sensors", "[Group] Temperature");
+    m_sensorNames[u"cpu/cpu\\d+/name"_s] = i18nc("All cores names", "[Group] Name");
 
-    m_sensorNames[QStringLiteral("partitions/(?!all).*/usedspace")] = i18n("[Group] Used");
-    m_sensorNames[QStringLiteral("partitions/(?!all).*/freespace")] = i18n("[Group] Available");
-    m_sensorNames[QStringLiteral("partitions/(?!all).*/filllevel")] = i18n("[Group] Percentage Used");
-    m_sensorNames[QStringLiteral("partitions/(?!all).*/total")] = i18n("[Group] Total Size");
+    m_sensorNames[u"partitions/(?!all).*/usedspace"_s] = i18n("[Group] Used");
+    m_sensorNames[u"partitions/(?!all).*/freespace"_s] = i18n("[Group] Available");
+    m_sensorNames[u"partitions/(?!all).*/filllevel"_s] = i18n("[Group] Percentage Used");
+    m_sensorNames[u"partitions/(?!all).*/total"_s] = i18n("[Group] Total Size");
 
-    m_sensorNames[QStringLiteral("network/(?!all).*/network")] = i18n("[Group] Network Name");
-    m_sensorNames[QStringLiteral("network/(?!all).*/ipv4address")] = i18n("[Group] IPv4 Address");
-    m_sensorNames[QStringLiteral("network/(?!all).*/ipv6address")] = i18n("[Group] IPv6 Address");
-    m_sensorNames[QStringLiteral("network/(?!all).*/signal")] = i18n("[Group] Signal Strength");
-    m_sensorNames[QStringLiteral("network/(?!all).*/download")] = i18n("[Group] Download Rate");
-    m_sensorNames[QStringLiteral("network/(?!all).*/downloadBits")] = i18n("[Group] Download Rate");
-    m_sensorNames[QStringLiteral("network/(?!all).*/totalDownload")] = i18n("[Group] Total Downloaded");
-    m_sensorNames[QStringLiteral("network/(?!all).*/upload")] = i18n("[Group] Upload Rate");
-    m_sensorNames[QStringLiteral("network/(?!all).*/uploadBits")] = i18n("[Group] Upload Rate");
-    m_sensorNames[QStringLiteral("network/(?!all).*/totalUpload")] = i18n("[Group] Total Uploaded");
+    m_sensorNames[u"network/(?!all).*/network"_s] = i18n("[Group] Network Name");
+    m_sensorNames[u"network/(?!all).*/ipv4address"_s] = i18n("[Group] IPv4 Address");
+    m_sensorNames[u"network/(?!all).*/ipv6address"_s] = i18n("[Group] IPv6 Address");
+    m_sensorNames[u"network/(?!all).*/signal"_s] = i18n("[Group] Signal Strength");
+    m_sensorNames[u"network/(?!all).*/download"_s] = i18n("[Group] Download Rate");
+    m_sensorNames[u"network/(?!all).*/downloadBits"_s] = i18n("[Group] Download Rate");
+    m_sensorNames[u"network/(?!all).*/totalDownload"_s] = i18n("[Group] Total Downloaded");
+    m_sensorNames[u"network/(?!all).*/upload"_s] = i18n("[Group] Upload Rate");
+    m_sensorNames[u"network/(?!all).*/uploadBits"_s] = i18n("[Group] Upload Rate");
+    m_sensorNames[u"network/(?!all).*/totalUpload"_s] = i18n("[Group] Total Uploaded");
 
-    m_sensorNames[QStringLiteral("disk/(?!all).*/name")] = i18n("[Group] Name");
-    m_sensorNames[QStringLiteral("disk/(?!all).*/total")] = i18n("[Group] Total Space");
-    m_sensorNames[QStringLiteral("disk/(?!all).*/used")] = i18n("[Group] Used Space");
-    m_sensorNames[QStringLiteral("disk/(?!all).*/free")] = i18n("[Group] Free Space");
-    m_sensorNames[QStringLiteral("disk/(?!all).*/read")] = i18n("[Group] Read Rate");
-    m_sensorNames[QStringLiteral("disk/(?!all).*/write")] = i18n("[Group] Write Rate");
-    m_sensorNames[QStringLiteral("disk/(?!all).*/usedPercent")] = i18n("[Group] Percentage Used");
-    m_sensorNames[QStringLiteral("disk/(?!all).*/freePercent")] = i18n("[Group] Percentage Free");
+    m_sensorNames[u"disk/(?!all).*/name"_s] = i18n("[Group] Name");
+    m_sensorNames[u"disk/(?!all).*/total"_s] = i18n("[Group] Total Space");
+    m_sensorNames[u"disk/(?!all).*/used"_s] = i18n("[Group] Used Space");
+    m_sensorNames[u"disk/(?!all).*/free"_s] = i18n("[Group] Free Space");
+    m_sensorNames[u"disk/(?!all).*/read"_s] = i18n("[Group] Read Rate");
+    m_sensorNames[u"disk/(?!all).*/write"_s] = i18n("[Group] Write Rate");
+    m_sensorNames[u"disk/(?!all).*/usedPercent"_s] = i18n("[Group] Percentage Used");
+    m_sensorNames[u"disk/(?!all).*/freePercent"_s] = i18n("[Group] Percentage Free");
 
     m_segmentNames[QLatin1String("cpu\\d+")] = i18n("[Group] CPU");
     m_segmentNames[QLatin1String("disk/(?!all).*")] = i18n("[Group] Disk");
